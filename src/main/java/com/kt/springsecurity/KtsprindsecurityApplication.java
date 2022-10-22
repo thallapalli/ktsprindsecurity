4e570cd9685d7f22a8f7a26e858a8239f8891932
thall <thall@host.docker.internal>
2022-10-21 19:41:25 -0500

    ignores

---
+++ src/main/java/com/kt/springsecurity/KtsprindsecurityApplication.java
@@ -0,0 +1,14 @@
+package com.kt.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtsprindsecurityApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from spring app loaded");
+		SpringApplication.run(KtsprindsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kt/springsecurity/KtsprindsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtsprindsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
